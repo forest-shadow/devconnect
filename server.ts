2fa7a67824950e5c57473bb92ba2bf0bf7c0b035
forest-shadow <mamasay@yandex.ru>
2019-03-06 22:48:44 +0300

    init commit

---
+++ server.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello'));
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+    console.log(`http://localhost:${port}`)
+  }
+);
