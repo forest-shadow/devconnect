fd4ddf5e68dea29a24112dde7c54b87db0719953
forest-shadow <mamasay@yandex.ru>
2019-03-07 08:51:53 +0300

    connect to mLab DB

--- server.ts
+++ server.ts
@@ -1,4 +1,15 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+console.warn(process.env.MONGODB_URI)
+
+mongoose
+  .connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.error(err))
 
 const app = express();
 
@@ -6,7 +17,7 @@ app.get('/', (req, res) => res.send('Hello'));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
-    console.log(`http://localhost:${port}`)
+  console.log(`Server running on port ${port}`)
+  console.log(`http://localhost:${port}`)
   }
 );
