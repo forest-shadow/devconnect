e6309119d9a13404333cd6846ba2e8a3d4af015a
forest-shadow <mamasay@yandex.ru>
2019-05-29 22:38:15 +0300

    Add `Profile` model

---
+++ models/Profile.ts
@@ -0,0 +1,97 @@
+import mongoose, { Schema } from 'mongoose'
+
+const ProfileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user'
+  },
+  company: {
+    type: String
+  },
+  website: {
+    type: String
+  },
+  location: {
+    type: String
+  },
+  status: {
+    type: String,
+    required: true
+  },
+  skills: {
+    type: [String],
+    required: true
+  },
+  bio: {
+    type: String
+  },
+  githubusername: {
+    type: String
+  },
+  experience: [
+    {
+      title: {
+        type: String,
+        required: true
+      },
+      company: {
+        type: String,
+        required: true
+      },
+      location: {
+        type: String
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String
+      }
+    }
+  ],
+  social: {
+    youtube: {
+      type: String
+    },
+    twitter: {
+      type: String
+    },
+    facebook: {
+      type: String
+    },
+    linkedin: {
+      type: String
+    },
+    instagram: {
+      type: String
+    }
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+interface IProfile extends mongoose.Document {
+  user: Schema.Types.ObjectId,
+  company: string,
+  website: string,
+  location: string,
+  status: string,
+  skills: string,
+  bio: string,
+  githubusername: string,
+  experience: [string|Date|boolean]
+}
+
+const Profile = mongoose.model<IProfile>('profile', ProfileSchema)
+
+export default Profile
