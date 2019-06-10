22c6277568c98167efb19e56d6bc1cc433123327
forest-shadow <mamasay@yandex.ru>
2019-06-10 18:43:46 +0300

    Use constant's name convention

--- server.ts
+++ server.ts
@@ -1,8 +1,8 @@
 import app from './app'
 
-const port = process.env.PORT || 5000
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
-  console.log(`http://localhost:${port}`)
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+  console.log(`http://localhost:${PORT}`)
   }
 )
