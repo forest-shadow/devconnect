d14b9bf1c934407fee577e89507f376593c2d9e1
forest-shadow <mamasay@yandex.ru>
2019-06-18 16:27:41 +0300

    Add Post model

---
+++ src/interfaces/post.ts
@@ -0,0 +1,26 @@
+import { User } from './user'
+import { Schema } from 'mongoose'
+
+export interface Post {
+  user: PostUser
+  text: string
+  name: string
+  avatar: string
+  likes: [PostLike]
+  comments: PostComment
+  date: Date
+}
+
+interface PostLike {
+  user: PostUser
+}
+
+interface PostComment {
+  user: PostUser
+  text: string
+  name: string
+  avatar: string
+  date: Date
+}
+
+declare type PostUser = string|User|Schema.Types.ObjectId
---
+++ src/models/Post.ts
@@ -0,0 +1,62 @@
+import mongoose from 'mongoose'
+
+import { Post } from '../interfaces/post'
+
+const Schema = mongoose.Schema
+
+const PostSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'user'
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String
+  },
+  avatar: {
+    type: String
+  },
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'user'
+      }
+    }
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'user'
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      name: {
+        type: String
+      },
+      avatar: {
+        type: String
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ],
+  date: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+interface PostDocument extends mongoose.Document, Post {}
+
+const PostModel = mongoose.model<PostDocument>('post', PostSchema)
+
+export default PostModel
