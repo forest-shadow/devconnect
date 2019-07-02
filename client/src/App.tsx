90364ff77c5eebd680b6407c88c6151c8ec9d89f
forest-shadow <mamasay@yandex.ru>
2019-07-02 12:03:39 +0300

    Add initial Store setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { Provider } from 'react-redux'
 
+import store from './store'
 import Navbar from './components/layout/Navbar'
 import Landing from './components/layout/Landing'
 import Register from './components/auth/Register'
@@ -11,18 +13,20 @@ import ROUTES from './constants/routes'
 import './App.css'
 
 const App: React.FC = () => (
-  <Router>
-    <>
-      <Navbar />
-      <Route exact path="/" component={Landing} />
-      <section className="container">
-        <Switch>
-          <Route path={ROUTES.LOGIN} component={Login} />
-          <Route path={ROUTES.REGISTER} component={Register} />
-        </Switch>
-      </section>
-    </>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <>
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <section className="container">
+          <Switch>
+            <Route path={ROUTES.LOGIN} component={Login} />
+            <Route path={ROUTES.REGISTER} component={Register} />
+          </Switch>
+        </section>
+      </>
+    </Router>
+  </Provider>
 )
 
 export default App
