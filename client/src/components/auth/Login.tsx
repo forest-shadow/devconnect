284a0f52e5726a63d4618c2e56913dd6da041d93
forest-shadow <mamasay@yandex.ru>
2019-07-02 11:39:07 +0300

    Add basic Login form

--- client/src/components/auth/Login.tsx
+++ client/src/components/auth/Login.tsx
@@ -1,7 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+import ROUTES from '../../constants/routes'
 
 const Login = () => {
-  return <div>Login</div>
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  })
+
+  const { email, password } = formData
+
+  const onChange = (e: React.FormEvent<HTMLInputElement>) =>
+    setFormData({ ...formData, [e.currentTarget.name]: e.currentTarget.value })
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    console.log(formData)
+  }
+
+  return (
+    <>
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Sign Into Your Account
+      </p>
+      <form className="form" onSubmit={e => onSubmit(e)}>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            value={email}
+            onChange={e => onChange(e)}
+            name="email"
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={e => onChange(e)}
+            required
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Login" />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to={ROUTES.REGISTER}>Sign Up</Link>
+      </p>
+    </>
+  )
 }
 
 export default Login
