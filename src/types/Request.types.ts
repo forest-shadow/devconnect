4961392deca9e853edca83802d7193f157f3fd50
forest-shadow <ivan.arevkov@gmail.com>
2023-03-27 17:25:24 +0200

    refactor(User.types) refactored User types

--- src/types/Request.types.ts
+++ src/types/Request.types.ts
@@ -1,5 +1,5 @@
 import { Request } from 'express'
-import { User } from '../interfaces/user'
+import { User } from './User.types'
 
 export interface AuthenticatedUserRequest extends Request {
   user: User
