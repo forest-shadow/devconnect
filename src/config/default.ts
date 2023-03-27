a1a661d60a2caa2848efe768a87840e6bcf30625
forest-shadow <ivan.arevkov@gmail.com>
2023-03-27 10:47:52 +0200

    refactor(db) updated db connection interface

--- src/config/default.ts
+++ src/config/default.ts
@@ -1,8 +1,7 @@
 export default {
   mongoURI: (user: string, password: string) => `mongodb+srv://${user}:${password}` +
-    `@devconnect-phapj.mongodb.net/test?retryWrites=true&w=majority`,
+    `@cluster0.d7aowhc.mongodb.net/?retryWrites=true&w=majority`,
   githubUri: (username: string, clientId: string, clientSecret: string) =>
     `https://api.github.com/users/${username}` +
     `/repos?per_page=5&sort=created:asc&client_id=${clientId}&client_secret=${clientSecret}`
-
 }
