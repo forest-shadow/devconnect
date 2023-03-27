00026b186f38c019440af2a10c4e984d35711930
forest-shadow <ivan.arevkov@gmail.com>
2023-03-27 13:17:55 +0200

    refactor(post) refactored post related middlewares to post and post comment controllers

--- src/routes/post.ts
+++ src/routes/post.ts
@@ -1,54 +1,42 @@
 import express from 'express'
 
 import API from '../constants/api'
-import {
-  createPostValidators,
-  createPostMiddleware
-} from './middleware/post/create'
-import { getPostsMiddleware, getPostMiddleware } from './middleware/post/get'
-import { deletePostMiddleware } from './middleware/post/delete'
-import {
-  likePostMiddleware,
-  unlikePostMiddleware
-} from './middleware/post/like'
-import {
-  createCommentValidators,
-  createCommentMiddleware,
-  deleteCommentMiddleware
-} from './middleware/post/comment'
+
 import { AuthController } from '../controllers/AuthController'
+import { PostController, createPostValidators } from '../controllers/PostController'
+import { PostCommentController, createCommentValidators } from '../controllers/PostCommentController'
 
 const router = express.Router()
 
 // @route   POST api/post
 // @desc    Create a post
 // @access  Public
-router.post('/', AuthController.tokenCheckout, createPostValidators, createPostMiddleware)
+router.post('/', AuthController.tokenCheckout, createPostValidators, PostController.create)
 
 // @route   GET api/post
 // @desc    Get all posts
 // @access  Private
-router.get('/', AuthController.tokenCheckout, getPostsMiddleware)
+router.get('/', AuthController.tokenCheckout, PostController.getAll)
 
 // @route   GET api/post/:post_id
 // @desc    Get post by id
 // @access  Private
-router.get(API.POST.GET_BY_ID, AuthController.tokenCheckout, getPostMiddleware)
+router.get(API.POST.GET_BY_ID, AuthController.tokenCheckout, PostController.getSingular)
 
 // @route   DELETE api/post/:post_id
 // @desc    Delete post by id
 // @access  Private
-router.delete(API.POST.DELETE, AuthController.tokenCheckout, deletePostMiddleware)
+router.delete(API.POST.DELETE, AuthController.tokenCheckout, PostController.delete)
 
 // @route   PUT api/post/:post_id/like
 // @desc    Like a post
 // @access  Private
-router.put(API.POST.ASSESS.LIKE, AuthController.tokenCheckout, likePostMiddleware)
+router.put(API.POST.ASSESS.LIKE, AuthController.tokenCheckout, PostController.like)
 
 // @route   PUT api/post/:post_id/unlike
 // @desc    Unlike a post
 // @access  Private
-router.put(API.POST.ASSESS.UNLIKE, AuthController.tokenCheckout, unlikePostMiddleware)
+router.put(API.POST.ASSESS.UNLIKE, AuthController.tokenCheckout, PostController.unlike)
 
 // @route   POST api/post/:post_id/comment
 // @desc    Comment on a post
@@ -57,12 +45,12 @@ router.post(
   API.POST.COMMENT.ADD,
   AuthController.tokenCheckout,
   createCommentValidators,
-  createCommentMiddleware
+  PostCommentController.create
 )
 
 // @route   Delete api/post/:post_id/comment/:comment_id
 // @desc    Delete comment on a post
 // @access  Private
-router.delete(API.POST.COMMENT.DELETE, AuthController.tokenCheckout, deleteCommentMiddleware)
+router.delete(API.POST.COMMENT.DELETE, AuthController.tokenCheckout, PostCommentController.delete)
 
 export default router
